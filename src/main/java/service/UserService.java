7ab1b280c81c87d79143e909bb68dd9b58851bab
nouran4200 <nouranm669@gmail.com>
2020-04-11 23:35:38 -0700

    user-service

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,14 @@
+package service;
+
+import entity.Product;
+import entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    int addUser(User user);
+    User getUserById(int userId);
+    List<User> getAllUsers();
+    void editUser(int userId, User user);
+    void deleteUser(int userId);
+}
