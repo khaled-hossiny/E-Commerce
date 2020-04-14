a887e0a33dfd1226788671922cc4b5cfea49e9db
khaled-barca <khaled.elhossiny10@gmail.com>
2020-04-14 19:10:13 +0200

    fixed buyer to cart relation

--- src/main/java/entity/Buyer.java
+++ src/main/java/entity/Buyer.java
@@ -8,44 +8,11 @@ import java.util.Set;
 
 @Entity
 public class Buyer extends User {
-//    private Integer id;
-//    private User userById;
-    private CreditCard creditCardById;
-    private Set<ShoppingCart> shoppingCartsById = new HashSet<>();
-    private Set<UserBuyProduct> userBuyProductsById = new HashSet<>();
 
-//    @Id
-//    @Column(name = "id", nullable = false)
-//    public Integer getId() {
-//        return id;
-//    }
-//
-//    public void setId(Integer id) {
-//        this.id = id;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Buyer buyer = (Buyer) o;
-//        return Objects.equals(id, buyer.id);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id);
-//    }
-//
-//    @OneToOne
-//    @JoinColumn(name = "id", referencedColumnName = "id", nullable = false)
-//    public User getUserById() {
-//        return userById;
-//    }
-//
-//    public void setUserById(User userById) {
-//        this.userById = userById;
-//    }
+    private CreditCard creditCardById;
+    private ShoppingCart shoppingCartsById;
+    private Set<UserBuyProduct> userBuyProductsById = new HashSet<>();
+    
 
     @OneToOne(mappedBy = "buyer")
     public CreditCard getCreditCardById() {
@@ -56,12 +23,12 @@ public class Buyer extends User {
         this.creditCardById = creditCardById;
     }
 
-    @OneToMany(mappedBy = "buyerByBuyerId")
-    public Set<ShoppingCart> getShoppingCartsById() {
+    @OneToOne(mappedBy = "buyerByBuyerId")
+    public ShoppingCart getShoppingCartsById() {
         return shoppingCartsById;
     }
 
-    public void setShoppingCartsById(Set<ShoppingCart> shoppingCartsById) {
+    public void setShoppingCartsById(ShoppingCart shoppingCartsById) {
         this.shoppingCartsById = shoppingCartsById;
     }
 
