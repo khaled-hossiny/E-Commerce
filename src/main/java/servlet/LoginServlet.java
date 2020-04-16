d4f7f3afd58807e2061b8a2f26f664b8a9be529d
nouran4200 <nouranm669@gmail.com>
2020-04-15 20:24:29 -0700

    merge with master

---
+++ src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,48 @@
+package servlet;
+import service.UserServiceImp;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+  //  private static final long serialVersionUID = 1L;
+    private UserServiceImp loginDao;
+
+    public void init() {
+        loginDao = new UserServiceImp();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.sendRedirect("login.jsp");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            authenticate(request, response);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void authenticate(HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (loginDao.validateLogin(username, password)) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher("login-success.jsp");
+            dispatcher.forward(request, response);
+        } else {
+            throw new Exception("Login not successful..");
+        }
+    }
+}
