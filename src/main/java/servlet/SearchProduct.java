432c78324bd2468844b977c77e2afd297e2af6e6
karima376 <karimamohamed988@gmail.com>
2020-04-19 02:25:48 +0200

    seearch

---
+++ src/main/java/servlet/SearchProduct.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import entity.Product;
+import service.UserServiceImp;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/search")
+public class SearchProduct extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        UserServiceImp userServiceImp = new UserServiceImp();
+        String name = req.getParameter("searchInput");
+        System.out.println("the input search is " +name);
+        List<Product> productList = userServiceImp.searchProduct(name);
+        for(Product pro : productList) {
+            System.out.println(pro.getName());
+        }
+
+    }
+}
