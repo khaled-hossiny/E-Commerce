2f952e46e6460c474ad2e293b5d523d634289a9a
khaled-barca <khaled.elhossiny10@gmail.com>
2020-04-20 18:10:45 +0200

    added register to filter

--- src/main/java/filter/LoginFilter.java
+++ src/main/java/filter/LoginFilter.java
@@ -18,8 +18,7 @@ public class LoginFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {
-        HttpServletRequest request = (HttpServletRequest) req;
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
         if (request.getRequestURI().matches(".*(css|jpg|png|gif|js)")) {
             chain.doFilter(request, response);
@@ -27,13 +26,16 @@ public class LoginFilter implements Filter {
         }
         HttpSession session = request.getSession(false);
         String loginURI = request.getContextPath() + "/login.jsp";
+        String registerURI = request.getContextPath() + "/Register.jsp";
         String loginServlet = request.getContextPath() + "/login";
+        String registerServlet = request.getContextPath() + "/register";
         System.out.println("Login URI : " + loginURI);
         boolean loggedIn = session != null && session.getAttribute("user") != null;
         System.out.println(loggedIn);
         boolean loginRequest = request.getRequestURI().equals(loginURI) || request.getRequestURI().equals(loginServlet);
+        boolean registerRequest = request.getRequestURI().equals(registerURI) || request.getRequestURI().equals(registerServlet);
         System.out.println("Request URI : " + request.getRequestURI());
-        if (loggedIn || loginRequest) {
+        if (loggedIn || loginRequest || registerRequest) {
             chain.doFilter(request, response);
         } else {
             System.out.println("not logged in");
